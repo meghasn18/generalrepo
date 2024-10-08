20f7f59c6a7dbafd21664f9c846bf2d6605841c1
meghasn18 <meghapjtm6@gmail.com>
2024-10-08 10:00:37 +0530

    first push

---
+++ TP07sample/src/test/java/package1/Sample1Test.java
@@ -0,0 +1,23 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	
+	@Test
+	public void orgtest()
+	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("create org executed");
+		
+	}
+
+}
---
+++ TP07sample/src/test/java/package2/Sample2Test.java
@@ -0,0 +1,13 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("create contact executed");
+	}
+
+}
