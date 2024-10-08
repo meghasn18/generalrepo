b52b11d7fb5d759dc544dd66d28dd76e627c5a92
meghasn18 <meghapjtm6@gmail.com>
2024-10-08 11:15:35 +0530

    modified sample2class

--- TP07sample/src/test/java/package2/Sample2Test.java
+++ TP07sample/src/test/java/package2/Sample2Test.java
@@ -5,8 +5,17 @@ import org.testng.annotations.Test;
 public class Sample2Test {
 	
 	@Test
-	public void demo2()
+	public void demo()
 	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("create contact executed");
 	}
 
